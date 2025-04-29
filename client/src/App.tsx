ba92fd7a0edf3b3ff4979f3f6c0541a30893bba0
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-04-29 19:40:45 -0400

    Revert "add routing with tanstack router"
    This reverts commit 93828657479ac14837858ca80223d2a14a74b046.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { BostonZipCodeMap } from "./map-test/maplibre/BostonZipCodeMap";
 
 function App() {
   return (
     <>
       <div className="App">This is my map App</div>
+      <BostonZipCodeMap />
     </>
   );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/boston-liquor-license-tracker/",
-  plugins: [
-    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react()],
+  plugins: [react()],
 })
