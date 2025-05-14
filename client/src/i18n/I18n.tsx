5d1c22e4b5a307454f3cf9c1063857a9696c4ad5
Haile Xiao <haile.xiao@gmail.com>
2025-05-13 21:31:11 -0400

    Default to closest locale match between browser and app

--- client/src/i18n/I18n.tsx
+++ client/src/i18n/I18n.tsx
@@ -1,11 +1,15 @@
 import { IntlProvider } from "react-intl";
-import { defaultLocale, supportedLocales } from "./i18n-config";
+import {
+  defaultLocale,
+  getBestMatchLocale,
+  supportedLocales,
+} from "./i18n-config";
 import { useState } from "react";
 import { LocaleContext } from "./locale-context";
 
 export default function I18n(props: React.PropsWithChildren) {
   const [locale, setLocale] =
-    useState<keyof typeof supportedLocales>(defaultLocale);
+    useState<keyof typeof supportedLocales>(getBestMatchLocale());
 
   return (
     <LocaleContext.Provider value={{ locale, setLocale }}>
--- client/src/i18n/i18n-config.ts
+++ client/src/i18n/i18n-config.ts
@@ -1,12 +1,13 @@
 import ES_ES_MESSAGES from "../locales/es-ES.json";
 import EN_US_MESSAGES from "../locales/en-US.json";
+import { match } from "@formatjs/intl-localematcher";
 
 // Default to American English when app loads, or when translation files are missing
-export const defaultLocale = "es-ES";
+export const defaultLocale = "en-US";
 
 export type Locale = keyof typeof supportedLocales;
 
-// Supported locales
+// Locales supported by the app
 export const supportedLocales = {
   "en-US": {
     name: "English",
@@ -18,3 +19,32 @@ export const supportedLocales = {
     messages: ES_ES_MESSAGES,
   },
 };
+
+// Locales supported by the browser
+export function getBrowserLocales(): string[] {
+  const browserLocales = [];
+
+  // User
+  if (navigator.languages) {
+    for (const language of navigator.languages) {
+      browserLocales.push(language);
+    }
+  }
+
+  if (navigator.language) {
+    browserLocales.push(navigator.language);
+  }
+
+  return browserLocales;
+}
+
+// Return the best match between the browser and the supported locales
+export function getBestMatchLocale(): Locale {
+  const browserLocales = getBrowserLocales();
+  const supported = Object.keys(supportedLocales);
+
+  // Match the best locale
+  const bestMatch = match(browserLocales, supported, defaultLocale);
+
+  return bestMatch as Locale;
+}
