4a9f019abbe090477df3a7d4126df2d2a89a008f
Haile Xiao <haile.xiao@gmail.com>
2025-05-20 19:03:52 -0400

    Add README to i18n section and use stored locale if we have it

--- client/src/i18n/i18n-config.ts
+++ client/src/i18n/i18n-config.ts
@@ -1,6 +1,7 @@
 import ES_ES_MESSAGES from "@/data/locales/es-ES.json";
 import EN_US_MESSAGES from "@/data/locales/en-US.json";
 import { match } from "@formatjs/intl-localematcher";
+import { getStoredLocale } from "./stored-locale";
 
 // Default to American English when app loads, or when translation files are missing
 export const defaultLocale = "en-US";
@@ -40,6 +41,12 @@ export function getBrowserLocales(): string[] {
 
 // Return the best match between the browser and the supported locales
 export function getBestMatchLocale(): Locale {
+  // If we have already stored a locale, use it
+  const storedLocale = getStoredLocale();
+  if (storedLocale) {
+    return storedLocale as Locale;
+  }
+
   const browserLocales = getBrowserLocales();
   const supported = Object.keys(supportedLocales);
 
