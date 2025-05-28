4752bd177588d94629cd60fc68183ba7a8b3e5de
dleibe1 <daniel.leibensperger28@gmail.com>
2025-05-27 20:56:14 -0400

    added translation to hero.tsx

--- client/src/components/pages/home/hero.tsx
+++ client/src/components/pages/home/hero.tsx
@@ -1,13 +1,18 @@
 import "./hero.css";
 import logo from "@/assets/logo.svg";
+import { FormattedMessage } from "react-intl";
+
 
 const Hero = () => {
   return (
     <div className="hero flex flex-col md:items-start md:justify-start w-full sm:w-screen pl-3 pr-3 pt-[100px] pb-[70px] lg:mt-[64px] md:pt-[200px] md:pb-[144px] md:pl-[64px] gap-[24px]">
       <img src={logo} className="w-2xl lg:w-5xl" alt="Logo" />
       <h2 className="text-white text-2xl md:text-3xl lg:text-5xl w-full font-bold">
-        Navigating the Liquor License system in Boston is hard. <br /> We are
-        here to make it just a little bit easier.
+        <FormattedMessage
+        id="hero.heading"
+					values={{
+						br: <br />,
+					}} />
       </h2>
       <p className="text-white text-xs md:text-base">
         Photo by{" "}
