b9a271e2f5117dfc16ce5fad6a456c79a3c7aceb
Daniel <dan.roder17@gmail.com>
2025-06-03 21:34:37 -0400

    refactor css to match specs

--- client/src/components/pages/database/license-type.tsx
+++ client/src/components/pages/database/license-type.tsx
@@ -3,14 +3,14 @@ import { FormattedMessage } from "react-intl";
 
 const LicenseType = () => {
   return (
-    <section className="license-type flex flex-col justify-center items-start m-[32px] max-w-[1440px] max-h-[640px]">
-      <h2 className="font-bold text-[36pt] m-0">
+    <section className="license-type p-[32px] bg-[#FDFDFD]">
+      <h2 className="font-bold text-[36px] m-0">
         <FormattedMessage
           id="license-type.header2"
           defaultMessage={"What does each type of license mean?"}
         />
       </h2>
-      <p className="mt-[8px] mb-[32px] font-normal text-[16]">
+      <p className="mt-[8px] mb-[32px] font-normal text-[16px]">
         <FormattedMessage
           id="license-type.h2-p"
           defaultMessage={
@@ -18,13 +18,13 @@ const LicenseType = () => {
           }
         />
       </p>
-      <h3 className="font-medium text-[32pt] m-0">
+      <h3 className="font-medium text-[32px]">
         <FormattedMessage
           id="license-type.transferable-h3"
           defaultMessage="Transferable Licenses"
         />
       </h3>
-      <p className="mt-[8px] mb-[32px] font-normal text-[16]">
+      <p className="mt-[8px] mb-[32px] font-normal text-[16px]">
         <FormattedMessage
           id="license-type.transferable-p"
           defaultMessage={
@@ -32,39 +32,39 @@ const LicenseType = () => {
           }
         />
       </p>
-      <h3 className="font-medium text-[32pt]">
+      <h3 className="font-medium text-[32px]">
         <FormattedMessage
           id="license-type.nonTransferable-h3"
           defaultMessage="Non-Transferable Licenses"
         />
       </h3>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16]">
+      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
         <FormattedMessage
           id="license-type.nonTransferable-p"
           defaultMessage="Restrictions are placed on certain licenses preventing them from being transferred outside of their legally designated area of Boston. These licenses can come in several different forms, most notably the licenses created under the 2024 law sedignating Licenses to Several Zip Codes."
         />
       </p>
-      <h4 className="font-medium text-[24pt]">
+      <h4 className="font-medium text-[24px]">
         <FormattedMessage
           id="license-type.law-h4"
           defaultMessage={"New 2024 Law"}
         />
       </h4>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16]">
+      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
         <FormattedMessage
           id="license-type.law-p"
           defaultMessage={
-            "In 2024 the city of Boston passed a new law to increase access to lquor licenses in several parts of Boston. Through this law the city will be issuing 5 new on-premise liquor licenses to 13 of Boston's Zip Codes each year for the next 3 years."
+            "In 2024 the city of Boston passed a new law to increase access to liquor licenses in several parts of Boston. Through this law the city will be issuing 5 new on-premise liquor licenses to 13 of Boston's Zip Codes each year for the next 3 years."
           }
         />
       </p>
-      <h4 className="font-medium text-[24pt]">
+      <h4 className="font-medium text-[24px]">
         <FormattedMessage
           id="license-type.specialAreas-h4"
           defaultMessage={"Special Areas of Boston"}
         />
       </h4>
-      <p className="mt-[8px] font-normal text-[16]">
+      <p className="mt-[8px] font-normal text-[16px]">
         <FormattedMessage
           id="license-type.specialAreas-p"
           defaultMessage={
