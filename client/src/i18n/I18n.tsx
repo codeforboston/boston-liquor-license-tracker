1dfc2a66c5d73301c95c143dab687b1666ff5c12
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-06-04 22:53:21 -0400

    Fallback to english for missing translations in a locale

--- client/src/i18n/I18n.tsx
+++ client/src/i18n/I18n.tsx
@@ -16,10 +16,22 @@ export default function I18n(props: React.PropsWithChildren) {
       <IntlProvider
         locale={locale}
         defaultLocale={defaultLocale}
-        messages={supportedLocales[locale].messages}
+        messages={getMessagesWithFallback(locale)}
       >
         {props.children}
       </IntlProvider>
     </LocaleContext.Provider>
   );
+}
+
+const getMessagesWithFallback = (locale: Locale) => {
+  const baseMessages = supportedLocales[defaultLocale].messages;
+  const localeMessages = supportedLocales[locale].messages;
+
+  if (locale === defaultLocale) {
+    return localeMessages;
+  } else {
+    // Merge locale messages on top of base messages
+    return { ...baseMessages, ...localeMessages };
+  }
 }
