d44ae8b8f5ab18fff98ff9d966dc5bb5145bb93c
filic2005 <ilic.f@northeastern.edu>
2025-06-06 01:09:06 -0400

    Implement License breakdown chart (ticket #70)

--- client/src/components/pages/database/database-view.tsx
+++ client/src/components/pages/database/database-view.tsx
@@ -1,12 +1,22 @@
 import "./database-view.css";
+import BreakdownChart from "./breakdown-chart";
 
 const DatabaseView = () => {
   return (
     <section className="database-view">
       <h1>Database View</h1>
       <p>This is where the database content will be displayed.</p>
+
+      {/* PLACEHOLDER */}
+      <BreakdownChart
+        totalLicenses={46}
+        transferableLicenses={44}
+        nonTransferableLicenses={8}
+        allAlcoholLicenses={46}
+        wineAndBeerLicenses={46}
+      />
     </section>
-  )
-}
+  );
+};
 
 export default DatabaseView;
