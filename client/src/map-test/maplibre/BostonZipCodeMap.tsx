aa9db04edb9d1e20a2d9066920bc8ee274141700
Celena Toon <16471076+seatuna@users.noreply.github.com>
2025-06-07 21:32:16 -0400

    map: add text to shapes

--- client/src/map-test/maplibre/BostonZipCodeMap.tsx
+++ client/src/map-test/maplibre/BostonZipCodeMap.tsx
@@ -2,22 +2,28 @@ import { useRef, useEffect } from "react";
 import maplibregl, { Map } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./BostonZipCodeMap.css";
-import { positron } from "../tileSources";
 import * as BostonZipCodeGeoJSON from "../../data/boston-zip-codes.json";
 
 export const BostonZipCodeMap = () => {
   const mapContainer = useRef(null);
   const map = useRef<Map | null>(null);
-  const lng = -71.0565;
-  const lat = 42.3555;
-  const zoom = 12;
+  const lng = -71.0782;
+  const lat = 42.3164;
+  const zoom = 11;
+
+  // {lng: -71.07826226470809, lat: 42.316413557174286}
 
   useEffect(() => {
     if (map.current) return; // stops map from intializing more than once
 
     map.current = new maplibregl.Map({
       container: mapContainer.current || "",
-      style: positron,
+      style: {
+        version: 8,
+        sources: {},
+        layers: [],
+        glyphs: "https://fonts.undpgeohub.org/fonts/{fontstack}/{range}.pbf",
+      },
       center: [lng, lat],
       zoom: zoom,
     });
@@ -49,6 +55,15 @@ export const BostonZipCodeMap = () => {
           "line-width": 2,
         },
       });
+      map.current.addLayer({
+        id: "symbols",
+        type: "symbol",
+        source: "boston",
+        layout: {
+          "text-field": ["get", "ZIP5"],
+          "text-font": ["Open Sans Regular"],
+        },
+      });
     });
     map.current.addControl(
       new maplibregl.NavigationControl({
@@ -72,6 +87,11 @@ export const BostonZipCodeMap = () => {
         const description = e.features?.[0].properties.ZIP5;
         popup.setLngLat(coordinates).setHTML(description).addTo(map.current);
       }
+      // centers to clicked
+      // map.current?.flyTo({
+      //   center: coordinates,
+      // });
+      console.log(map.current?.getCenter());
     });
   }, [lng, lat, zoom]);
 
