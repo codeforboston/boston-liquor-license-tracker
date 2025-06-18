462b58f8c38ce547611c4d5c5a7b7964c1466e7a
Haile Xiao <haile.xiao@gmail.com>
2025-06-17 22:44:03 -0400

    Scrape next meeting date from boston.gov

--- client/src/components/pages/database/header.tsx
+++ client/src/components/pages/database/header.tsx
@@ -1,8 +1,38 @@
 import { FormattedMessage } from "react-intl";
 import "./header.css";
+import * as cheerio from "cheerio";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-// TODO: Replace with actual logic for getting the next meeting date
-const nextMeetingDate = null;
+async function scrapeNextMeetingDate(url: string): Promise<Date | null> {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+  const currentYearElement = $("section#content")
+    .find(".paragraphs-item-drawers")
+    .first()
+    .find(
+      `.paragraphs-item-drawer .field.field-label-hidden div:contains('${currentYear}')`
+    )
+    .parentsUntil(".section-drawers")
+    .find(".entity .field ul");
+
+  const currentDateStrings = currentYearElement
+    .text()
+    .split("\n")
+    .filter((dateString) => !!dateString && dateString.includes("Voting"))
+    .map((dateString) => dateString.replace(/\(Voting\)/g, "").trim());
+
+  const meetingDates = currentDateStrings.map(
+    (dateString) => new Date(`${dateString}, ${currentYear}`)
+  );
+
+  const nextMeetingDate =
+    meetingDates.find((date) => date > currentDate) ?? null;
+
+  return nextMeetingDate;
+}
 
 // Show the next meeting date if and only if we have one and it is in the future
 const getNextMeetingText = (nextMeeting: Date | null) => {
@@ -25,6 +55,20 @@ const getNextMeetingText = (nextMeeting: Date | null) => {
 };
 
 const Header = () => {
+  const [nextMeetingDate, setNextMeetingDate] = useState(
+    new Date("2000-01-01")
+  );
+  useEffect(() => {
+    async function fetchNextMeetingDate() {
+      const date = await scrapeNextMeetingDate(
+        "https://www.boston.gov/departments/licensing-board/licensing-board-information-and-members"
+      );
+      if (date) {
+        setNextMeetingDate(date);
+      }
+    }
+    fetchNextMeetingDate();
+  }, []);
   return (
     <header className="database-header">
       <div className="text-container">
