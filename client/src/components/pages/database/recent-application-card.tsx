52597861be03a3f1eb53e2e2a3e5e8b09ade4b83
Will Worrell <willrequaworrell@gmail.com>
2025-06-24 23:59:17 -0400

    font styling updates per Griffin feedback

--- client/src/components/pages/database/recent-application-card.tsx
+++ client/src/components/pages/database/recent-application-card.tsx
@@ -2,28 +2,30 @@ import statusApprovedIcon from "@/assets/status-approved.svg"
 import statusPendingIcon from "@/assets/status-pending.svg"
 import statusDeniedIcon from "@/assets/status-denied.svg"
 
-interface RecentApplicationCardPropsType {
-    applicationData: {
-        dbaName: string
-        businessName: string;
-        address: string;
-        licenseNumber: string;
-        licenseType: string;
-        applicationDate: string;
-        status: "approved" | "pending" | "denied"
-    }
+interface ApplicationData {
+    dbaName: string
+    businessName: string;
+    address: string;
+    licenseNumber: string;
+    licenseType: string;
+    applicationDate: string;
+    status: "approved" | "pending" | "denied" // not sure how the statuses come through from the DB
+}
+
+interface RecentApplicationCardProps {
+    applicationData: ApplicationData
 }
 
 /* For Testing */
-const dummyData = {
+const dummyData: ApplicationData = {
     businessName: "Business Name",
     dbaName: "Dba Name",
     address: "Street Address, Neighborhood, MA ZIP Code",
     licenseNumber: "License Number",
     licenseType: "License Type",    
     applicationDate: "App. Date",
-    status: "denied"
-}
+    status: "pending"
+} 
 
 const statusIconMap = {
     approved: {icon: statusApprovedIcon, bgColor: "bg-[#46C800]"},
@@ -31,7 +33,7 @@ const statusIconMap = {
     denied: {icon: statusDeniedIcon, bgColor: "bg-[#FF1111]"},
 }
 
-const RecentApplicationCard = ({applicationData=dummyData}: RecentApplicationCardPropsType) => {
+const RecentApplicationCard = ({applicationData=dummyData}: RecentApplicationCardProps) => {
     return (
         <article className="
             relative
@@ -45,13 +47,14 @@ const RecentApplicationCard = ({applicationData=dummyData}: RecentApplicationCar
             px-[16px]
             py-[8px]
             bg-[#f2f2f2] 
+            text-[#2E2E2E]
             rounded-[8px] 
             shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)]"
         >
             <header className=" font-bold">
-                <h5 className="text-[14px] leading-[14px]">{applicationData.businessName}</h5>
+                <h5 className="text-[12px] font-semibold">{applicationData.businessName}</h5>
             </header>
-            <div className="flex flex-col gap-y-[4px] text-[12px] leading-[12px] font-normal italic">
+            <div className="flex flex-col gap-y-[4px] text-[10px] font-light italic">
                 <p>{applicationData.dbaName}</p>
                 <p>{applicationData.address}</p>
                 <p>{applicationData.licenseNumber}</p>
@@ -61,7 +64,7 @@ const RecentApplicationCard = ({applicationData=dummyData}: RecentApplicationCar
             <img 
                 src={statusIconMap[applicationData.status].icon} 
                 alt={`status ${applicationData.status} icon`} 
-                className={`absolute top-[8px] right-[8px] size-[18px] p-[2px] rounded-full ${statusIconMap[applicationData.status].bgColor}`}
+                className={`absolute shrink-0 top-[8px] right-[8px] size-[18px] p-[2px] rounded-full ${statusIconMap[applicationData.status].bgColor}`}
             />
         </article>
     )
