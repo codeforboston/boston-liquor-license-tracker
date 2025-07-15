eb70e0160ef7b433270e767ecc487ac60d4d2e25
Will Worrell <willrequaworrell@gmail.com>
2025-07-15 00:11:37 -0400

    completed extra work section with extra work card pulled out into component

--- client/src/components/pages/home/extra-work.tsx
+++ client/src/components/pages/home/extra-work.tsx
@@ -1,13 +1,18 @@
+import ExtraWorkCard from "./extra-work-card";
 import "./extra-work.module.css";
+import { extraWorkData } from "@/data/extra-work-data";
 
 const ExtraWork = () => {
-  return (
-    <div className="extra-work">
-      <div className="w-full">
-        <h1>Extra Work</h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className="extra-work p-[32px] overflow-x-scroll">
+			<h2 className="text-2xl font-bold">Check out more of our work!</h2>
+			<div className="flex w-full pt-[16px] gap-x-[64px]">
+				{extraWorkData.map(item => (
+					<ExtraWorkCard item={item}/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default ExtraWork;
