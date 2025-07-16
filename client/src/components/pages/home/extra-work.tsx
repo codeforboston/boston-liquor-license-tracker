2fe371b2e96c3f824968512b27bf29d495dd2147
Will Worrell <willrequaworrell@gmail.com>
2025-07-15 22:21:44 -0400

    add internationalization, remove unused css  module import

--- client/src/components/pages/home/extra-work.tsx
+++ client/src/components/pages/home/extra-work.tsx
@@ -1,11 +1,13 @@
-import ExtraWorkCard from "./extra-work-card";
-import "./extra-work.module.css";
+import { FormattedMessage } from "react-intl";
 import { extraWorkData } from "@/data/extra-work-data";
+import ExtraWorkCard from "./extra-work-card";
 
 const ExtraWork = () => {
 	return (
 		<div className="extra-work p-[32px] overflow-x-scroll">
-			<h2 className="text-2xl font-bold">Check out more of our work!</h2>
+			<h2 className="text-2xl font-bold">
+            <FormattedMessage id="home.extra-work.h2" />
+         </h2>
 			<div className="flex w-full pt-[16px] gap-x-[64px]">
 				{extraWorkData.map((item, index) => (
 					<ExtraWorkCard key={index} item={item}/>
