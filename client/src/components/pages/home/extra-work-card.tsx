29a5f429a7e1cefbe8ca31fd0ec85fb9d6f97dbf
Will Worrell <willrequaworrell@gmail.com>
2025-07-15 22:12:08 -0400

    update space formatting + add key to extra work cards

--- client/src/components/pages/home/extra-work-card.tsx
+++ client/src/components/pages/home/extra-work-card.tsx
@@ -1,16 +1,16 @@
 import { ExtraWork } from "@/data/extra-work-data"
 
 interface ExtraWorkCardProps {
-    item: ExtraWork
+  item: ExtraWork
 }
 
 
-const ExtraWorkCard = ({item}: ExtraWorkCardProps) => {
-    return (
-        <a 
-            href={item.href}
-            target="_blank"
-            className={`
+const ExtraWorkCard = ({ item }: ExtraWorkCardProps) => {
+  return (
+    <a
+      href={item.href}
+      target="_blank"
+      className={`
                 flex
                 items-end
                 shrink-0
@@ -22,13 +22,13 @@ const ExtraWorkCard = ({item}: ExtraWorkCardProps) => {
                 bg-no-repeat
                 cursor-pointer
             `}
-            style={{ backgroundImage: `url(${item.imgSrc})`}}
-        >
-            <p className={`text-[24px] text-shadow-lg font-semibold ${item.textColor === "light" ? "text-black" : "text-white"}`}>
-                {item.title}
-            </p>
-        </a>
-    )
+      style={{ backgroundImage: `url(${item.imgSrc})` }}
+    >
+      <p className={`text-[24px] text-shadow-lg font-semibold ${item.textColor === "light" ? "text-black" : "text-white"}`}>
+        {item.title}
+      </p>
+    </a>
+  )
 }
 
 export default ExtraWorkCard
--- client/src/data/extra-work-data.ts
+++ client/src/data/extra-work-data.ts
@@ -3,29 +3,29 @@ import bootcampImg from "@/assets/images/bootcamp.jpeg"
 import cfbLogo from "@/assets/images/cfb-logo.png"
 
 export interface ExtraWork {
-    title: string
-    imgSrc: string
-    href: string
-    textColor: "light" | "dark"
+  title: string
+  imgSrc: string
+  href: string
+  textColor: "light" | "dark"
 }
 
 export const extraWorkData: ExtraWork[] = [
-    {
-        title: "OFFSITE Hospitality",
-        imgSrc: offsiteLogo,
-        href: "https://www.getoffsite.com/",
-        textColor: "dark"
-    },
-    {
-        title: "Bar Management Bootcamp",
-        imgSrc: bootcampImg,
-        href: "https://www.getoffsite.com/bootcamp",
-        textColor: "dark"
-    },
-    {
-        title: "Code for Boston",
-        imgSrc: cfbLogo,
-        href: "https://www.codeforboston.org/",
-        textColor: "light"
-    },
+  {
+    title: "OFFSITE Hospitality",
+    imgSrc: offsiteLogo,
+    href: "https://www.getoffsite.com/",
+    textColor: "dark"
+  },
+  {
+    title: "Bar Management Bootcamp",
+    imgSrc: bootcampImg,
+    href: "https://www.getoffsite.com/bootcamp",
+    textColor: "dark"
+  },
+  {
+    title: "Code for Boston",
+    imgSrc: cfbLogo,
+    href: "https://www.codeforboston.org/",
+    textColor: "light"
+  },
 ]
