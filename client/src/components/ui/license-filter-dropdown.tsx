60d06608137933f4ab054471aa2331107bcf6757
Will Worrell <willrequaworrell@gmail.com>
2025-08-06 23:38:36 -0400

    complete basic version with no option styling, including placeholder text capability, added material icons package

--- client/src/components/ui/license-filter-dropdown.tsx
+++ client/src/components/ui/license-filter-dropdown.tsx
@@ -1,19 +1,61 @@
+import { ChangeEvent, useState } from 'react'
+import { ExpandMore } from '@mui/icons-material';
 
-const LicenseFilterDropdown = () => {
+
+interface DropdownOptionProps {
+  option: string
+  isPlaceholder?: boolean
+}
+
+interface LicenseFilterDropdownProps {
+  options: string[]
+  placeholderOption?: string 
+}
+
+
+const DropdownOption = ({option, isPlaceholder=false}: DropdownOptionProps) => {
+  return (
+    <option 
+      disabled={isPlaceholder}
+      hidden={isPlaceholder}
+      value={isPlaceholder ? "" : option}
+    >
+      {option}
+    </option>
+  )
+}
+
+const LicenseFilterDropdown = ({options, placeholderOption}: LicenseFilterDropdownProps) => {
+  const [selected, setSelected] = useState<string>(placeholderOption ? "" : options[0])
+  const fullOptions = placeholderOption ? [placeholderOption, ...options] : options
+  
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value)
+  }
+
+  console.log(selected)
   return (
     <div 
-      className="inline-flex h-[40px] w-[160px] px-[16px] py-[8px] rounded-[8px] bg-[#F2F2F2] ">
+      className={`filter-container relative inline-flex h-[40px] w-[160px] px-[16px] py-[8px] rounded-[8px] bg-[#F2F2F2]`}
+    >
       <select
         name="license-dropdown-filter" 
         id="license-dropdown-filter"
-        className="border-none w-full"
+        value={selected}
+        className={`appearance-none bg-[#F2F2F2] w-full text-[14px]`}
+        onChange={handleSelect}
       >
-        <option disabled selected hidden>Select an Option</option>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-        <option value="4">Option 4</option>
+        {fullOptions.map((opt, idx) =>  (
+          <DropdownOption 
+            key={idx} 
+            option={opt} 
+            isPlaceholder={idx === 0 && !!placeholderOption}
+          />
+        ))}
       </select>
+      <span className='icon-container absolute right-[16px] pointer-events-none pl-[8px] border-l-[.5px] border-[#2e2e2e]'>
+          <ExpandMore id="expand-more" style={{ fontSize: 24, color: '#2e2e2e' }}/>
+      </span>
     </div>
   )
 }
