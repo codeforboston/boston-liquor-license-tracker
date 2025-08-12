ee9cce73982c923b16c085a710727df8a8533418
Will Worrell <willrequaworrell@gmail.com>
2025-08-12 15:36:43 -0400

    cleaned up leftover log button, extra css files, and reworked placeholder option logic for simplicity per feedback

--- client/src/components/ui/license-filter-dropdown.tsx
+++ client/src/components/ui/license-filter-dropdown.tsx
@@ -1,60 +1,43 @@
 import { ChangeEvent, useState } from 'react'
 import { ExpandMore } from '@mui/icons-material';
 
-
-interface DropdownOptionProps {
-  option: string
-  isPlaceholder?: boolean
-}
-
 interface LicenseFilterDropdownProps {
   options: string[]
-  placeholderOption?: string 
+  placeholderOption?: string
 }
 
 
-const DropdownOption = ({option, isPlaceholder=false}: DropdownOptionProps) => {
-  return (
-    <option 
-      disabled={isPlaceholder}
-      hidden={isPlaceholder}
-      value={isPlaceholder ? "" : option}
-    >
-      {option}
-    </option>
-  )
-}
-
-const LicenseFilterDropdown = ({options, placeholderOption}: LicenseFilterDropdownProps) => {
+const LicenseFilterDropdown = ({ options, placeholderOption }: LicenseFilterDropdownProps) => {
   const [selected, setSelected] = useState<string>(placeholderOption ? "" : options[0])
-  const fullOptions = placeholderOption ? [placeholderOption, ...options] : options
-  
+
   const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     setSelected(e.target.value)
   }
 
-  console.log(selected)
   return (
-    <div 
+    <div
       className={`filter-container relative inline-flex h-[40px] w-[160px] px-[16px] py-[8px] rounded-[8px] bg-[#F2F2F2]`}
     >
       <select
-        name="license-dropdown-filter" 
+        name="license-dropdown-filter"
         id="license-dropdown-filter"
         value={selected}
         className={`appearance-none bg-[#F2F2F2] w-full text-[14px]`}
         onChange={handleSelect}
       >
-        {fullOptions.map((opt, idx) =>  (
-          <DropdownOption 
-            key={idx} 
-            option={opt} 
-            isPlaceholder={idx === 0 && !!placeholderOption}
-          />
+        {placeholderOption &&
+          <option disabled hidden value={""}>
+            {placeholderOption}
+          </option>
+        }
+        {options.map((opt, idx) => (
+          <option key={idx} value={opt}>
+            {opt}
+          </option>
         ))}
       </select>
       <span className='icon-container absolute right-[16px] pointer-events-none pl-[8px] border-l-[.5px] border-[#2e2e2e]'>
-          <ExpandMore id="expand-more" style={{ fontSize: 24, color: '#2e2e2e' }}/>
+        <ExpandMore id="expand-more" style={{ fontSize: 24, color: '#2e2e2e' }} />
       </span>
     </div>
   )
