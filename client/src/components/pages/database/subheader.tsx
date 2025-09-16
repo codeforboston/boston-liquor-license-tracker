8b93d6680914eef1f75e5d4984c764f734e3b0de
Jasmine Rose <72074190+jasrose@users.noreply.github.com.>
2025-09-16 18:34:06 -0400

    i18n(Database): translate subheader.tsx and add locale strings

--- client/src/components/pages/database/subheader.tsx
+++ client/src/components/pages/database/subheader.tsx
@@ -1,12 +1,13 @@
 import styles from "./subheader.module.css";
+import { FormattedMessage } from "react-intl";
 
 const SubHeader = () => {
   return (
     <section className={styles.subheader}>
-      <h2 className={styles.heading}>License Availability Lookup:</h2>
-      <p className={styles.description}>
-        Select a Zip Code below to see the available licenses in each area. Use the filters narrow or expand your search to meet your exact business needs
-      </p>
+      <h2 className={styles.heading}>
+        <FormattedMessage id="database.subheader.title" defaultMessage="License Availability Lookup:"/></h2>
+      <p className={styles.description}>       
+        <FormattedMessage  id="database.subheader.description" defaultMessage="Select a Zip Code below to see the available licenses in each area. Use the filters to narrow or expand your search to meet your exact business needs."/></p>
     </section>
   );
 };
