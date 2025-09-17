0405208bc0c623ef0e90336f98b599ee32558c32
Matthew Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-09-16 20:03:47 -0400

    Update client/src/components/pages/database/subheader.tsx

--- client/src/components/pages/database/subheader.tsx
+++ client/src/components/pages/database/subheader.tsx
@@ -5,7 +5,8 @@ const SubHeader = () => {
   return (
     <section className={styles.subheader}>
       <h2 className={styles.heading}>
-        <FormattedMessage id="database.subheader.title" defaultMessage="License Availability Lookup:"/></h2>
+        <FormattedMessage id="database.subheader.title"/>
+     </h2>
       <p className={styles.description}>       
         <FormattedMessage  id="database.subheader.description" defaultMessage="Select a Zip Code below to see the available licenses in each area. Use the filters to narrow or expand your search to meet your exact business needs."/></p>
     </section>
