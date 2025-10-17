4b296630e5b930b489f3fd6660b7e265cac29724
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-10-17 00:10:03 -0400

    Update formatted messages from licenseType to licenseTerminology

--- client/src/components/pages/database/license-type.tsx
+++ client/src/components/pages/database/license-type.tsx
@@ -5,34 +5,34 @@ const LicenseType = () => {
   return (
     <section className="license-type py-[32px] px-[64px] bg-light">
       <h2 className="font-bold text-[36px] m-0">
-        <FormattedMessage id="database.licenseType.header2" />
+        <FormattedMessage id="database.licenseTerminology.header" />
       </h2>
       <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
-        <FormattedMessage id="database.licenseType.header2-paragraph" />
-      </p>
-      <h3 className="font-medium text-[32px]">
-        <FormattedMessage id="database.licenseType.transferable-header2" />
-      </h3>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
-        <FormattedMessage id="database.licenseType.transferable-paragraph" />
-      </p>
-      <h3 className="font-medium text-[32px]">
-        <FormattedMessage id="database.licenseType.nonTransferable-header3" />
-      </h3>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
-        <FormattedMessage id="database.licenseType.nonTransferable-paragraph" />
+        <FormattedMessage id="database.licenseTerminology.description" />
       </p>
       <h4 className="font-medium text-[24px]">
-        <FormattedMessage id="database.licenseType.law-header4" />
-      </h4>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
-        <FormattedMessage id="database.licenseType.law-paragraph" />
-      </p>
-      <h4 className="font-medium text-[24px]">
-        <FormattedMessage id="database.licenseType.specialAreas-header4" />
+        <FormattedMessage id="database.licenseTerminology.allAlcohol.title" />
       </h4>
       <p className="mt-[8px] font-normal text-[16px]">
-        <FormattedMessage id="database.licenseType.specialAreas-paragraph" />
+        <FormattedMessage id="database.licenseTerminology.allAlcohol.description" />
+      </p>
+      <h4 className="font-medium text-[24px]">
+        <FormattedMessage id="database.licenseTerminology.wineAndBeer.title" />
+      </h4>
+      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+        <FormattedMessage id="database.licenseTerminology.wineAndBeer.description" />
+      </p>
+      <h3 className="font-medium text-[32px]">
+        <FormattedMessage id="database.licenseTerminology.transferable.title" />
+      </h3>
+      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+        <FormattedMessage id="database.licenseTerminology.transferable.description" />
+      </p>
+      <h3 className="font-medium text-[32px]">
+        <FormattedMessage id="database.licenseTerminology.nonTransferable.title" />
+      </h3>
+      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+        <FormattedMessage id="database.licenseTerminology.nonTransferable.description" />
       </p>
     </section>
   );
