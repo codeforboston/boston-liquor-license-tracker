13f5e05660b0aa6901b012df26b417b1d2864ce9
rann143 <dan.roder17@gmail.com>
2025-10-28 19:10:54 -0400

    define formatData function outside of component

--- client/src/components/pages/database/license-availability-table.tsx
+++ client/src/components/pages/database/license-availability-table.tsx
@@ -6,6 +6,25 @@ import { validateBusinessLicense, getAvailableLicensesByZipcode, BusinessLicense
 import { MAX_ALL_ALC_PER_ZIP, MAX_AVAILABLE_PER_ZIP, MAX_BEER_WINE_PER_ZIP } from '@/services/data-interface/data-interface';
 import { RowWithSubRows } from '@components/ui/table';
 
+const formatData = (data: BusinessLicense[], zipcodeList: Set<EligibleBostonZipcode>) => {
+    const zips = [...zipcodeList]
+    const d = zips.map((zipcode) => {
+    const {totalAvailable, allAlcoholAvailable, beerWineAvailable} = getAvailableLicensesByZipcode(data, zipcode); 
+    const entry = {
+        rowData: [zipcode, String(totalAvailable), '-', String(MAX_AVAILABLE_PER_ZIP - totalAvailable), String(MAX_AVAILABLE_PER_ZIP)],
+        subRowData: [
+            ['All Alcohol Licenses', String(allAlcoholAvailable), '-', String(MAX_ALL_ALC_PER_ZIP - allAlcoholAvailable), String(MAX_ALL_ALC_PER_ZIP)],
+            ['Beer & Wine Licenses', String(beerWineAvailable), '-', String(MAX_BEER_WINE_PER_ZIP - beerWineAvailable), String(MAX_BEER_WINE_PER_ZIP)],
+        ] 
+    }
+
+    return entry as RowWithSubRows;
+
+    })
+
+    return d;
+  }
+
 const LicenseAvailabilityTable = () => {
     const [data, setData] = useState<BusinessLicense[]>([]);
 
@@ -33,34 +52,11 @@ const LicenseAvailabilityTable = () => {
     'Total Licenses'
   ]
 
-
-  
-  const formatData = (data: BusinessLicense[], zipcodeList: Set<EligibleBostonZipcode>) => {
-      const zips = [...zipcodeList]
-      const d = zips.map((zipcode) => {
-       const {totalAvailable, allAlcoholAvailable, beerWineAvailable} = getAvailableLicensesByZipcode(data, zipcode); 
-       const entry = {
-           rowData: [zipcode, String(totalAvailable), '-', String(MAX_AVAILABLE_PER_ZIP - totalAvailable), String(MAX_AVAILABLE_PER_ZIP)],
-           subRowData: [
-             ['All Alcohol Licenses', String(allAlcoholAvailable), '-', String(MAX_ALL_ALC_PER_ZIP - allAlcoholAvailable), String(MAX_ALL_ALC_PER_ZIP)],
-             ['Beer & Wine Licenses', String(beerWineAvailable), '-', String(MAX_BEER_WINE_PER_ZIP - beerWineAvailable), String(MAX_BEER_WINE_PER_ZIP)],
-           ] 
-       }
-
-       return entry as RowWithSubRows;
-
-      })
-
-      return d;
-  }
-
-
   const formattedData = formatData(data, eligibleBostonZipcodes);
 
  if (formattedData == null) {
      return null
  }
-
 
   return (
     <section className="license-availability-table">
