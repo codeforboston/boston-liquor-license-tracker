79cbda326aa7603993fdd7090678a9d7f8687627
madeleine-b <madeleine-b@users.noreply.github.com>
2026-01-21 22:01:36 -0500

    scrolling nice

--- client/src/components/ui/dot-pagination.tsx
+++ client/src/components/ui/dot-pagination.tsx
@@ -1,12 +1,19 @@
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { PaginationArrow } from './Pagination';
+import { useRef } from 'react';
 
-type PaginationProps = {
+type DotPaginationProps = {
   currentPage: number;
   totalPages: number;
   indexToZipCodes: {[key: int]: string};
   onPageChange: (page: number) => void;
+}
+
+type DotButtonProps = {
+  id: number;
+  isSelected: boolean;
+  onSelect: (number) => void;
 }
 
 // TIL JavaScript does not have a real modulo operator
@@ -15,12 +22,28 @@ Number.prototype.mod = function(n) {
   return ((this%n)+n)%n;
 }
 
-function DotPagination({ currentPage, totalPages, onPageChange, indexToZipCodes }: PaginationProps) {
+function DotButton({id, isSelected, onSelect}: DotButtonProps) {
+  return (
+    <div>
+      <button
+        key={id}
+        id={id}
+        onClick={() => onSelect(id)}
+        className={
+            `border border-[2px] h-[20px] min-w-[20px] cursor-pointer rounded-[100px] 
+            ${isSelected ? "border-[1px] border-background-dark bg-button-default-dark text-font-light" 
+            : "border-button-hovered-light hover:bg-button-hovered-light"}`}
+          / >
+    </div>
+  );
+}
+
+function DotPagination({ currentPage, totalPages, onPageChange, indexToZipCodes }: DotPaginationProps) {
 
   const pageNumbers = [...Array(totalPages).keys()];
 
   return (
-    <div className='flex gap-[4px]'>
+    <div style={{overflow: "hidden"}} className='flex gap-[4px]'>
       <PaginationArrow
         isDisabled={false}
         onClick={() => onPageChange((currentPage - 1).mod(totalPages))}
@@ -29,20 +52,11 @@ function DotPagination({ currentPage, totalPages, onPageChange, indexToZipCodes 
           fill: "var(--color-button-default-dark)"
         }} />
       </PaginationArrow>
-      <div className="justify-center overflow-y-scroll items-center gap-[4px]">
-      {pageNumbers.map((page) => (
-        <button
-          key={indexToZipCodes[page]}
-          onClick={() => onPageChange(page)}
-          className={
-            `border border-[2px] h-[32px] min-w-[32px] cursor-pointer rounded-[4px] 
-            ${currentPage === page ? "border-[1px] border-background-dark bg-button-default-dark text-font-light" 
-            : "border-button-hovered-light hover:bg-button-hovered-light"}`}
-        >
-          {page + 1}
-        </button>
-      ))}
-    </div>
+      <div style={{overflow: "hidden", gap: "4px", display: "flex", whitespace: "nowrap"}}>
+        {pageNumbers.map((page) => (
+          <DotButton id={page} isSelected={currentPage === page} onSelect={onPageChange} />
+        ))}
+      </div>
       <PaginationArrow
         isDisabled={false}
         onClick={() => onPageChange((currentPage + 1).mod(totalPages))}
