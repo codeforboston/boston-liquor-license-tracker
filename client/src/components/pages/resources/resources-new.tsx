df435250541966a05654cba539d9911db033ce37
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2026-09-08 20:17:49 -0400

    Added header to new resources page

--- client/src/components/pages/resources/resources-new.tsx
+++ client/src/components/pages/resources/resources-new.tsx
@@ -1,9 +1,16 @@
 // New resources page being refactored (in progress.)
+import { useIntl } from "react-intl";
+import Header from "./header";
 
 const ResourcesNew = () => {
+  const intl = useIntl();
+  const title = `${intl.formatMessage({ id: "resources.pageTitle" })} | ${intl.formatMessage({ id: "home.pageTitle" })}`;
 
   return (
-    <main>New resources page</main>
+    <main>
+      <title>{title}</title>
+      <Header />
+    </main>
   );
 };
 
